fbf3744dd0f73d0150e43f2edcb96bbb7dea987d
majiahao <1367485513@qq.com>
2019-06-10 17:43:31 +0800

    6.10.3-output sym_name,almost complete

---
+++ try_printf_output_formot.c
@@ -0,0 +1,11 @@
+printf("  name=%s\n",flinfo->symstrtab->array[i]->root->string);
+printf("  name=%s\n",(flinfo->symstrtab)->array[i]->root->string);
+
+
+printf("third:name=%s\n",flinfo.symstrtab->array[i]->root->string);
+
+
+printf("third:num=%d,name=%25s\n",idx,entry->root.string);
+ p flinfo->symstrtab->array[1]->root.string
+
+ printf(" name=%s\n",flinfo->symstrtab->array[elfsym->sym.st_name]->root.string);
